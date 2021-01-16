004d8605513c3f19485a2a02c7902454070a0217
akvile <akvile.bobinaite@gmail.com>
2021-01-16 22:51:39 +0200

    Initial commit

---
+++ Domain/Value.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Domain
+{
+    public class Value
+    {   
+        // get;set -- it means that this property can be getted or setted
+        public int Id { get; set; } //Id named property automaticly will be used as pk (primary key)
+        public int Name { get; set; }
+    }
+}
