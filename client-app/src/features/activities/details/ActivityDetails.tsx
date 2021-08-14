c3d605f71c12b954202d2a6483000be0a47d02be
akvile <akvile.bobinaite@gmail.com>
2021-08-14 19:01:07 +0300

    Add some styling.

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,10 +1,14 @@
 import { observer } from "mobx-react-lite";
 import React, { useContext } from "react";
 import { useEffect } from "react";
-import { Link, RouteComponentProps } from "react-router-dom";
-import { Button, Card, Image } from "semantic-ui-react";
+import { RouteComponentProps } from "react-router-dom";
+import { Grid } from "semantic-ui-react";
 import { LoadingComponent } from "../../../app/layout/LoadingComponent";
 import ActivityStore from "../../../app/stores/activityStore";
+import ActivityDetailedSidebar from "./ActivitiDetailedSidebar";
+import ActivityDetailedChat from "./ActivityDetailedChat";
+import ActivityDetailedHeader from "./ActivityDetailedHeader";
+import ActivityDetailedInfo from "./ActivityDetailedInfo";
 
 interface DetailParams {
   id: string;
@@ -12,7 +16,6 @@ interface DetailParams {
 
 const ActivityDetails: React.FC<RouteComponentProps<DetailParams>> = ({
   match,
-  history,
 }) => {
   const activityStore = useContext(ActivityStore);
   const { activity, loadActivity, loadingInitial } = activityStore;
@@ -25,38 +28,48 @@ const ActivityDetails: React.FC<RouteComponentProps<DetailParams>> = ({
     return <LoadingComponent content="Loading activity..." />;
 
   return (
-    <Card fluid>
-      <Image
-        src={`/assets/categoryImages/${activity!.category}.jpg`}
-        wrapped
-        ui={false}
-      />
-      <Card.Content>
-        <Card.Header>{activity!.title}</Card.Header>
-        <Card.Meta>
-          <span>{activity!.date}</span>
-        </Card.Meta>
-        <Card.Description>{activity!.description}</Card.Description>
-      </Card.Content>
-      <Card.Content extra>
-        <Button.Group widths={2}>
-          <Button
-            // onClick={() => openEditForm(activity!.id)}
-            as={Link}
-            to={`/manage/${activity.id}`}
-            basic
-            color="blue"
-            content="Edit"
-          />
-          <Button
-            onClick={() => history.push("/activities")}
-            basic
-            color="grey"
-            content="Cancel"
-          />
-        </Button.Group>
-      </Card.Content>
-    </Card>
+    <Grid>
+      <Grid.Column width={10}>
+        <ActivityDetailedHeader activity={activity} />
+        <ActivityDetailedInfo activity={activity} />
+        <ActivityDetailedChat />
+      </Grid.Column>
+      <Grid.Column width={6}>
+        <ActivityDetailedSidebar />
+      </Grid.Column>
+    </Grid>
+    // <Card fluid>
+    //   <Image
+    //     src={`/assets/categoryImages/${activity!.category}.jpg`}
+    //     wrapped
+    //     ui={false}
+    //   />
+    //   <Card.Content>
+    //     <Card.Header>{activity!.title}</Card.Header>
+    //     <Card.Meta>
+    //       <span>{activity!.date}</span>
+    //     </Card.Meta>
+    //     <Card.Description>{activity!.description}</Card.Description>
+    //   </Card.Content>
+    //   <Card.Content extra>
+    //     <Button.Group widths={2}>
+    //       <Button
+    //         // onClick={() => openEditForm(activity!.id)}
+    //         as={Link}
+    //         to={`/manage/${activity.id}`}
+    //         basic
+    //         color="blue"
+    //         content="Edit"
+    //       />
+    //       <Button
+    //         onClick={() => history.push("/activities")}
+    //         basic
+    //         color="grey"
+    //         content="Cancel"
+    //       />
+    //     </Button.Group>
+    //   </Card.Content>
+    // </Card>
   );
 };
 
